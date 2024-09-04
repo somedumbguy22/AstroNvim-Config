a604c1ec8292170fcc7c7c90e5d61fd0845a6ef9
somedumbguy22 <aakash.sham@gmail.com>
2024-09-04 14:20:06 -0400

    added in the debugger config for nvim-dap to work with elixir and phoenix apps. The debugger does launch but then errors out on set exception breakpoints (nothing to do with user set break points). There's some additional config required or additonal setup required to fully use this, but information on the internet is limited. Will return to this later

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,27 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require "dap"
+    dap.adapters.mix_task = {
+      type = "executable",
+      command = "/home/aakash/.local/share/nvim/mason/packages/elixir-ls/debug_adapter.sh",
+      args = {},
+    }
+
+    dap.configurations.elixir = {
+      {
+        type = "mix_task",
+        name = "mix test",
+        task = "test",
+        taskArgs = { "--trace" },
+        request = "launch",
+        startApps = true, -- for Phoenix projects
+        projectDir = "${workspaceFolder}",
+        requireFiles = {
+          "test/**/test_helper.exs",
+          "test/**/*_test.exs",
+        },
+      },
+    }
+  end,
+}
