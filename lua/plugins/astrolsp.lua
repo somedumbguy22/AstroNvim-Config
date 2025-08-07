47ffd377543aa8e65e44cbd53714eb2b8de5c401
somedumbguy22 <aakash.sham@gmail.com>
2025-08-07 14:58:55 -0400

    removed autoformatting on markdown files

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -25,6 +25,7 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
+          "markdown",
           -- "python",
         },
       },
